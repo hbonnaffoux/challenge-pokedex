08ca6f2e26435607a8c4b2964fc18e2998e60a8a
Herve Bonnaffoux <bonnaffouxherve@gmail.com>
2021-11-26 11:54:37 +0100

    first commit

--- src/components/pokemon_card.js
+++ src/components/pokemon_card.js
@@ -1,13 +1,50 @@
 import React, {useState, useEffect} from "react";
-import { Card, PokemonName, PokemonType, PokemonId } from "./styles/Card.styled";
+import styled from "styled-components";
+
 
 export const PokemonCard = ({pokemon}) => {
     return (
         <Card>
-        <img src={pokemon.sprites.front_default}></img>
+        <PokemonImage src={pokemon.sprites.front_default}></PokemonImage>
         <PokemonId>{pokemon.id}</PokemonId>
         <PokemonName>{pokemon.name}</PokemonName>
         <PokemonType> {pokemon.types.map(type => (type.name)).join(' | ') } </PokemonType>
         </Card>
     );
 };
+
+
+const Card = styled.div`
+width: 200px;
+padding: 10px 20px;
+margin: 0 auto;
+margin-bottom: 20px;
+border-radius: 5px;
+box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+transition: 0.3s;
+&:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+  }
+`
+
+const PokemonImage = styled.img`
+width: 200px;
+height:200px;
+`
+
+const PokemonName = styled.div`
+font-size: 26px;
+text-align:center;
+font-weight: bolder;
+margin-bottom: 15px;
+`
+
+const PokemonType = styled.div`
+font-size: 16px;
+text-align:center;
+`
+
+const PokemonId = styled.div`
+font-size: 16px;
+text-align:center;`
+
--- src/containers/pokemons_container.js
+++ src/containers/pokemons_container.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { ALL_POKEMON } from "../graphql/all_pokemon";
 import { PokemonCard } from "../components/pokemon_card";
-import { Container } from "../components/styles/Container.styled";
-import { Header } from "../components/styles/Header.styled";
+import styled from "styled-components";
+
 
 export const PokemonsContainer = () => {
   const [input, setInput] = useState('')
@@ -22,7 +22,7 @@ export const PokemonsContainer = () => {
     <div>
       <Header>Pokedex     
       </Header>
-      <input type="text "  onChange={(event) => setInput(event.currentTarget.value)}/>
+      <Search><input type="text "  onChange={(event) => setInput(event.currentTarget.value)}/></Search>
       <Container>
         {allPokemon.map((pokemon) => {
           return pokemon.name.includes(input) && (
@@ -33,3 +33,37 @@ export const PokemonsContainer = () => {
     </div>
   )
 };
+
+const Header = styled.div`
+text-align: center;
+background-color: #B22222;
+color: white;
+height: 80px;
+font-size:60px;
+font-family: Lobster, cursive;
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+`
+
+const Container = styled.div`
+width: 900px;
+max-width: 100%;
+padding: 0 10px;
+margin: 0 auto;
+margin-top: 40px;
+display: grid;
+grid-template-columns: repeat(3, 1fr);
+
+@media (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);  }
+
+@media (max-width: 540px) {
+    grid-template-columns: repeat(1, 1fr);  }
+`
+const Search = styled.div`
+text-align: center;
+height: 80px;
+margin-top: 30px;
+`
